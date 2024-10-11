c8493226753aef52095cbd0261e08b5648a20236
Alessio006 <alessio-giuseppe.marotta@latori.com>
2024-10-11 14:16:00 +0200

    I made the grid adjustable

--- java.js
+++ java.js
@@ -1,15 +1,35 @@
+const body = document.querySelector("body")
 const container = document.getElementById("container")
+let gritsize =16
+const gritsziebnt = document.createElement("button")
+gritsziebnt.appendChild(document.createTextNode("Change Grit size"))
+gritsziebnt.addEventListener("click",function() {
+    container.innerHTML = "";
+        gritsize=prompt("Put in the size of the git (1 to 100)");
+    if(gritsize>100||gritsize<1){
+        alert("Please only put in a number betwean 1 and 100");
+        return;
+    }
+    let divwith = 960  / gritsize;
+    
+    
+        gritsize = gritsize * gritsize
+        for(let i=0;i<gritsize;i++){
+            const newdiv = document.createElement("div")
+            newdiv.classList.add("box")
+            newdiv.id = (i)
+            newdiv.style.width = divwith+"px";
+            newdiv.style.height = divwith+"px";
+            container.appendChild(newdiv)
+        }
+    
+    
+    for(let i=0;i<gritsize;i++){
+        const div =document.getElementById(i);
+        div.addEventListener("mouseenter", function() {
+            div.style.backgroundColor = "black";
+        });
+    }
 
-
-for(let i=0;i<256;i++){
-    const newdiv = document.createElement("div")
-    newdiv.classList.add("box")
-    newdiv.id = (i)
-    container.appendChild(newdiv)
-}
-for(let i=0;i<256;i++){
-    const div =document.getElementById(i);
-    div.addEventListener("mousemove", function() {
-        div.style.backgroundColor = "black";
-    });
-}
+});
+body.appendChild(gritsziebnt)
